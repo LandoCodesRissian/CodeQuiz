5eb40b67dbeff07a42bc787c2558bb5681f7c11a
LandonP172 <141693593+LandonP172@users.noreply.github.com>
2023-09-17 20:49:46 -0600

    added all code now that laptop is working again

---
+++ assets/script.js
@@ -0,0 +1,183 @@
+const startButton = document.getElementById('start-button');
+const quizQuestions = document.getElementById('quiz-questions');
+const timer = document.getElementById('timer');
+const gameOverScreen = document.getElementById('game-over');
+const initialsInput = document.getElementById('initials');
+const saveScoreButton = document.getElementById('save-score');
+const highScoreSection = document.getElementById('high-scores');
+const quizScores = document.getElementById('quiz-scores');
+const retakeBtn = document.getElementById('retake');
+const clearScoresBtn = document.getElementById('clear-scores');
+
+
+let timeLeft = 60; 
+
+    // Quiz questions
+const questions = [
+    {   //q1
+        question: 'What does the "DOM" stand for in JavaScript?',
+        options: ['Document Object Model', 'Data Object Model', 'Document Oriented Model', 'Domino'],
+        correctAnswer: 'Document Object Model',
+    },
+    {   //q2
+        question: 'What is the correct way to declare a JavaScript variable?',
+        options: ['var name;', 'variable name;', 'v name;', 'declare name;'],
+        correctAnswer: 'var name;',
+    },
+    {   //q3
+        question: 'Which of the following is not a JavaScript data type?',
+        options: ['String', 'Boolean', 'Float', 'Undefined'],
+        correctAnswer: 'Float',
+    },
+    {   //q4
+        question: 'What is the result of 5 + "5" in JavaScript?',
+        options: ['10', '55', '5 + "5"', 'Error'],
+        correctAnswer: '55',
+    },
+    {   //q5
+        question: 'What is the purpose of the JavaScript `console.log()` method?',
+        options: ['To display a message box', 'To write data to a file', 'To print output to the console', 'To create an alert'],
+        correctAnswer: 'To print output to the console',
+    },
+    {   //q6
+        question: 'How do you declare a "Function" in Javascript?',
+        options: ['Func()', 'Function functionName', 'function functionName() {}', 'function variableName'],
+        correctAnswer: 'function functionName() {}', 
+    }
+    
+    //need to add more questions(shooting for 10)
+];
+
+let countdown;
+let currentQuestionIndex = 0;
+
+// Function to start the quiz
+function startQuiz() {
+    startButton.style.display = 'none';
+    timer.textContent = 'Time: ' + timeLeft + ' seconds';
+    displayNextQuestion();
+    startTimer();
+}
+
+// Function to display the next question
+function displayNextQuestion() {
+    if (currentQuestionIndex < questions.length) 
+    {
+        const question = questions[currentQuestionIndex];
+        const optionsContainer = document.createElement('div');
+
+        // Display the question text
+        quizQuestions.innerHTML = '<p>' + question.question + '</p>';
+
+        // Display answer options
+        question.options.forEach((option) => {
+            const optionButton = document.createElement('button');
+            optionButton.textContent = option;
+
+            // Add an event listener to handle the answer
+            optionButton.addEventListener('click', () => {
+                answerQuestion(option);
+            });
+
+            optionsContainer.appendChild(optionButton);
+        });
+
+        quizQuestions.appendChild(optionsContainer);
+    } 
+    else 
+    {
+        endGame();
+    }
+}
+
+
+// Function to start the timer
+function startTimer() {
+    countdown = setInterval(function () {
+        timeLeft--;
+        if (timeLeft === 1 ) {
+            timer.textContent = 'Time: ' + timeLeft + ' second';
+        }
+        else {
+            timer.textContent = 'Time: ' + timeLeft + ' seconds';
+        }
+
+        if (timeLeft <= 0) {
+            clearInterval(countdown);
+            endGame();
+        }
+    }, 1000);
+}
+
+// Function to handle when a question is answered
+function answerQuestion(selectedOption) {
+    const question = questions[currentQuestionIndex];
+
+    if (selectedOption != question.correctAnswer)
+    {
+        if (timeLeft < 10) 
+        {
+            timeLeft === 0
+        } 
+        else 
+        {
+            timeLeft -= 10;
+        }
+    }
+
+    currentQuestionIndex++;
+    displayNextQuestion();
+}
+function questionsAnswered() {
+    return currentQuestionIndex === questions.length;
+}
+function checkQuiz() {
+    if (questionsAnswered() || timeLeft <=0) {
+        endGame()
+    } 
+}
+// Function to end the game
+function endGame() {
+    clearInterval(countdown);
+    quizQuestions.style.display = 'none';
+    gameOverScreen.style.display = 'block';
+}
+
+// Event listeners
+startButton.addEventListener('click', startQuiz);
+saveScoreButton.addEventListener('click', saveScore);
+retakeBtn.addEventListener('click', retakeQuiz);
+clearScoresBtn.addEventListener('click', clearScores);
+// You can add event listeners for answering questions here
+function saveScore(){
+
+    var existingScores = JSON.parse(localStorage.getItem("highScores"));
+    if(existingScores == null) existingScores = [];
+    var score = {
+        initials: document.getElementById("initials").value,
+        score: timeLeft
+    };
+    localStorage.setItem("score", JSON.stringify(score));
+
+    existingScores.push(score);
+    localStorage.setItem("highScores", JSON.stringify(existingScores));
+
+    existingScores.forEach((score) => {
+        const scoresContainer = document.createElement('div');
+        scoresContainer.innerHTML = '<p>' + score.initials + ' ' + score.score + '</p>';
+        quizScores.appendChild(scoresContainer);
+    });
+
+    timer.style.display = 'none';
+    gameOverScreen.style.display = 'none';
+    highScoreSection.style.display = 'block';
+}
+
+function retakeQuiz() {
+    location.reload();
+}
+
+function clearScores() {
+    localStorage.clear();
+    quizScores.textContent = "";
+} 
